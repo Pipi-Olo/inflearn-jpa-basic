6d8378b06ecf7bb5657bc82b2edc51d8af75d265
pipiolo <Copy.H13@gmail.com>
2022-06-18 15:59:36 +0900

    Jpql Domain 추가

---
+++ jpql/src/main/java/jpql/Team.java
@@ -0,0 +1,49 @@
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+    private Integer age;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+}
